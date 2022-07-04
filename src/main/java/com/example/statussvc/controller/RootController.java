ac87905cec4ba0729e3e5f5cf128a8fa3a857acd
Yurii Polishchuk <ypolishchuk@roku.com>
2022-07-04 18:06:02 +0300

    Project Initial Setup#1

--- src/main/java/com/example/statussvc/controller/RootController.java
+++ src/main/java/com/example/statussvc/controller/RootController.java
@@ -1,8 +1,6 @@
 package com.example.statussvc.controller;
 
 import com.example.statussvc.Constants;
-import com.example.statussvc.configuration.ApplicationProperties;
-import com.example.statussvc.wire.response.TestResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
@@ -10,9 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * Entry point for Root endpoint APIs.
@@ -22,17 +17,14 @@ import java.util.Map;
 @RequestMapping(Constants.URL_SEPARATOR)
 public class RootController {
 
-    private final ApplicationProperties applicationProperties;
-
     /**
      * Handle root endpoint.
      *
      * @return ResponseEntity with proper HttpStatus
      */
     @GetMapping
-    public ResponseEntity<TestResponse> rootEndpoint() {
-        TestResponse build = TestResponse.builder().test2("test").testMap(Map.of("test1", "test2")).listStrings(List.of("test")).build();
-        return new ResponseEntity<>(build, HttpStatus.OK);
+    public ResponseEntity<String> rootEndpoint() {
+        return new ResponseEntity<>(StringUtils.EMPTY, HttpStatus.OK);
     }
 
 }
