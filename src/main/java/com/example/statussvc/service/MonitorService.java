21e16bb5e9ee096ad2d6bd08ab49941d53b55dfe
Yurii Polishchuk <ypolishchuk@roku.com>
2022-07-05 21:52:31 +0300

    Project Initial Setup (code for tasks)

---
+++ src/main/java/com/example/statussvc/service/MonitorService.java
@@ -0,0 +1,19 @@
+package com.example.statussvc.service;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * Generic Monitor Service
+ */
+@Log4j2
+public class MonitorService {
+
+    /**
+     * Add docs
+     */
+    public void checkHostsAvailability() {
+        log.info("checking...");
+        // Implement
+    }
+
+}
