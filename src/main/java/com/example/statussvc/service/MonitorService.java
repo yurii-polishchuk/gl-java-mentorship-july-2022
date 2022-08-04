04b9e59c0da2de23a68477c044bf154df4563062
Anton Tsarenko <58445366+AntonTsa@users.noreply.github.com>
2022-08-04 13:27:09 +0300

    [CRON] implement basic scheduler job for running task (#26)
    Updated Schedule Task and test

--- src/main/java/com/example/statussvc/service/MonitorService.java
+++ src/main/java/com/example/statussvc/service/MonitorService.java
@@ -1,16 +1,22 @@
 package com.example.statussvc.service;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 /**
  * Generic Monitor Service
  */
 @Log4j2
+@Service
+@EnableScheduling
 public class MonitorService {
 
     /**
      * Add docs
      */
+    @Scheduled(cron = "${application.scheduler.host.availability.cron}")
     public void checkHostsAvailability() {
         log.info("checking...");
         // Implement
